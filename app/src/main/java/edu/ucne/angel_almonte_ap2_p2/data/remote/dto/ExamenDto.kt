c2b1abdb14f93e1484b0f0d94660337ab7972f34
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-03-11 18:17:58 -0400

    Para iniciar el examen

--- app/src/main/java/edu/ucne/angel_almonte_ap2_p2/data/remote/dto/ExamenDto.kt
+++ app/src/main/java/edu/ucne/angel_almonte_ap2_p2/data/remote/dto/ExamenDto.kt
@@ -1,4 +1,16 @@
 package edu.ucne.angel_almonte_ap2_p2.data.remote.dto
 
-class ExamenDto {
+import edu.ucne.angel_almonte_ap2_p2.domain.model.Deposito
+
+data class JugadorRequest(
+    val nombres: String,
+    val email: String
+
+)
+data class JugadorDto(
+
+) {
+    fun toDomain() = Deposito(
+
+    )
 }
--- app/src/main/java/edu/ucne/angel_almonte_ap2_p2/domain/model/jugador.kt
+++ app/src/main/java/edu/ucne/angel_almonte_ap2_p2/domain/model/jugador.kt
@@ -1,4 +1,5 @@
 package edu.ucne.angel_almonte_ap2_p2.domain.model
 
-class examen {
-}
+data class jugador(
+
+)
